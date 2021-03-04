caa64a5e61373ab227c66a66599c2211786d8f3e
smackem <10erSaiten>
2021-03-04 17:07:11 +0100

    - added basic structure

--- Waddle.Core/Symbols/Lexer.cs
+++ Waddle.Core/Symbols/Lexer.cs
@@ -1,4 +1,11 @@
-namespace Waddle.Core.Syntax {
+namespace Waddle.Core.Symbols {
+    /// <summary>
+    /// The lexer turns the input source code into a stream of tokens. A token represents an atomic syntactical element,
+    /// for example a string literal, an identifier, a number, the keyword `if` or the operator `+`.
+    /// Usually a lexer is implemented as a state machine with one state per symbol class (word, number, operator,
+    /// comment etc).
+    /// Alternative implementations use regular expressions with a performance penalty.
+    /// </summary>
     public class Lexer {
     }
 }
