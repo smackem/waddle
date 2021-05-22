b5a86d0e8ec580b42220422e014867363c10fdd8
Lasse Clausen <lasseclausen@hotmail.de>
2021-05-22 18:47:07 +0200

    -new testcase + Update Numbers test

--- Waddle.Test/LexerTest.cs
+++ Waddle.Test/LexerTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using Waddle.Core.Symbols;
 using Xunit;
 
@@ -8,10 +9,38 @@ namespace Waddle.Test {
         [Fact]
         public void Numbers() {
             using var reader = new CharReader(new StringReader(
-                @"abc\n123 blah\n!@$+"));
+                "abc\n123 blah\n!@$+"));
             using var lexer = new Lexer(reader);
-            Assert.Collection(lexer.Lex(),
-    t => Assert.Equal(TokenType.Number, t.Type));
+            foreach (var token in lexer.Lex())
+            {
+                Assert.True(token.Type is TokenType.Number or TokenType.Identifier or TokenType.Plus);
+            }
+        }
+        
+        [Fact]
+        public void LexerTest123() {
+            using var reader = new CharReader(new StringReader(
+                @"// `let` declares a binding which cannot be assigned to.
+
+function addTwoNumbers(a: int, b: int) -> int {
+    return a + b;
+}
+
+function getGreaterNumber(a: int, b: int) -> int {
+    if (a > b) {
+        return a;
+    }
+    return b;
+}
+
+function main() {
+    var x: int = addTwoNumbers(1, 2);
+    var y: int = getGreaterNumber(x, 100);
+    print(x, y);
+}"));
+            using var lexer = new Lexer(reader);
+            var sut = lexer.Lex().ToList();
+            Assert.NotEmpty(sut);
         }
     }
 }
